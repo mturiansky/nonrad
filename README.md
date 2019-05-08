4b609c677d066a960cc6f09dcdd0b9b9cd452c28
Mark Turiansky <mark.turiansky@gmail.com>
2019-05-08 14:22:05 -0700

    initial setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# NONRAD
+
+An implementation of the methodology pioneered by Alkauskas *et al.* for computing nonradiative recombination rates from first principles.
+
+### Installation
+### Usage
+### Contributing
+### How to Cite
+### Acknowledgements
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.1'
+
+with open('README.md', 'r') as f:
+    long_desc = f.read()
+
+setup(
+    name='nonrad',
+    version=VERSION,
+    author='Mark E. Turiansky',
+    author_email='mturiansky@physics.ucsb.edu',
+    description='',
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mturiansky/nonrad',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy'],
+    keywords=['physics', 'materials', 'science', 'VASP', 'recombination',
+              'Shockley-Read-Hall'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
