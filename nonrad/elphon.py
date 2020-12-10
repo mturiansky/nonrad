Fix missing eigenvalue difference calculation
At present, `deig` actually corresponds to the bulk_index eigenvalues, rather than the difference in eigenvalues between the bulk and defect.
Tested and seems to work as expected. (I noticed this from sanity testing with a manual calculation of Wif using the WSWQ, dQ and eigenvalues, which was giving me different results at first, but this fixed it).

nonrad/elphon.py
@@ -286,9 +286,10 @@ def get_Wif_from_WSWQ(
 
     # first compute the eigenvalue differences
     bvr = BSVasprun(initial_vasprun)
+    sp = Spin.up if spin == 0 else Spin.down
+    def_eig = bvr.eigenvalues[sp][kpoint-1][def_index-1][0]
     for i, bi in enumerate(bulk_index):
-        sp = Spin.up if spin == 0 else Spin.down
-        deig[i] = bvr.eigenvalues[sp][kpoint-1][bi-1][0]
+        deig[i] = bvr.eigenvalues[sp][kpoint-1][bi-1][0] - def_eig
     deig = np.abs(deig)
 
     # now compute for each Q